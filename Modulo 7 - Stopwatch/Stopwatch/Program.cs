6f87d03e9b1e31992ec245b1d4dec6ef72fa498f
Delciney <delciney07@gmail.com>
2021-06-22 12:03:32 -0300

    Criando o Start

--- Modulo 7 - Stopwatch/Stopwatch/Program.cs
+++ Modulo 7 - Stopwatch/Stopwatch/Program.cs
@@ -34,15 +34,15 @@ namespace Stopwatch
 
             int time = int.Parse(data.Substring(0, data.Length - 1));
             char type = char.Parse(data.Substring(data.Length - 1, 1));
-            int mutiplier = 1;
+            int multiplier = 1;
 
             if (time == 'm')
-                mutiplier = 60;
+                multiplier = 60;
 
-            PreStart();
+            PreStart(time * multiplier);
         }
 
-        static void PreStart()
+        static void PreStart(int time)
         {
             Console.Clear();
 
@@ -55,7 +55,25 @@ namespace Stopwatch
             Console.WriteLine("Go...");
             Thread.Sleep(1000);
 
-            // chama a função
+            Start(time);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while(currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+            Menu();
         }
     }
 }
