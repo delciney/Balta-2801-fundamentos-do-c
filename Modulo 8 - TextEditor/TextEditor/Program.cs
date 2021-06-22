79153733268a7e757c5b57ab89aac2e1dbae3bb3
Delciney <delciney07@gmail.com>
2021-06-22 16:39:17 -0300

    Iniciando o projeto

---
+++ Modulo 8 - TextEditor/TextEditor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu() 
+        {
+            Console.Clear();
+        }
+        static void Open() 
+        {
+            Console.Clear();
+
+        }
+        static void Edit() 
+        {
+            Console.Clear();
+
+        }
+        static void Save() 
+        {
+            Console.Clear();
+
+        }
+    }
+}
