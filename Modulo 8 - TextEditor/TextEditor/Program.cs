c8e7700291250df33ee7de48d596ba8c7c901ee7
Delciney <delciney07@gmail.com>
2021-06-22 17:00:26 -0300

    Criando o Menu

--- Modulo 8 - TextEditor/TextEditor/Program.cs
+++ Modulo 8 - TextEditor/TextEditor/Program.cs
@@ -13,6 +13,21 @@ namespace TextEditor
         static void Menu() 
         {
             Console.Clear();
+
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("0 - Sair");
+
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Open(); break;
+                case 2: Edit(); break;
+                default: Menu(); break;
+            }
         }
         static void Open() 
         {
