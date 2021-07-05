e8ac7ff4b3d1a188fdd8913ee960c686f5a35c04
Delciney <delciney07@gmail.com>
2021-07-05 10:31:23 -0300

    Guids

--- Modulo 9 - Strings/Program.cs
+++ Modulo 9 - Strings/Program.cs
@@ -6,7 +6,23 @@ namespace Strings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ExemploGuids();
+        }
+
+        static void ExemploGuids()
+        {
+            var id = Guid.NewGuid();
+
+            // errado
+            //id = new Guid("");
+
+            // correto
+            //id = new Guid("15942b04-5b6b-424d-8d64-4ff3170e3849");
+
+            //id = new Guid();
+            // retorna 00000000-0000-0000-0000-000000000000
+
+            Console.WriteLine(id);
         }
     }
 }
